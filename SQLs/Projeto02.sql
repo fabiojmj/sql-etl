ee5b2b9c97ecf3b71f8c1d5ee3d6492255838b1c
Fabio Mendonca <fabio.jose.mendonca@gmail.com>
2020-09-07 20:44:31 -0300

    aula 28

---
+++ SQLs/Projeto02.sql
@@ -0,0 +1,9 @@
+USE PRJ_INTEGRATION
+GO
+
+--SELECT * FROM PRODUTOS
+
+--SELECT * FROM N_PROMOCAO
+
+--insert into PRODUTOS(Produto,Categoria,Valor,Promocao) values('Iphone 7','Eletronicos','3000.00','N');
+
